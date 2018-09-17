b4521065fc7d16de3cc7cfbb1c8d2d0221337799
Daniel Fegan <daniel.fegan@moneysupermarket.com>
2018-09-17 19:09:07 +0100

    bring in apache commons csv library. read from CSV file and output profiles to JSON

--- src/main/java/Entry.java
+++ src/main/java/Entry.java
@@ -1,31 +1,13 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Entry {
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws IOException {
         ProfileDao profileDao = new ProfileDao();
         List<Profile> profiles = new ArrayList<>();
-        createProfiles(profiles);
+        profileDao.getProfilesFromCSV(profiles);
         profiles.forEach(profile -> profileDao.save(profile));
-    }
-
-    private static List<Profile> createProfiles(List<Profile> profiles) {
-        profiles.add(new Profile(
-            new Address("test1", "test1", "test1", "test1", "test1"),
-            new Customer("Dan", "Fegan"),
-            new Car("E57 VKD", "Ford", "Focus", 2.4)));
-
-        profiles.add(new Profile(
-            new Address("test2", "test2", "test2", "test2", "test2"),
-            new Customer("Tom", "Jones"),
-            new Car("E57 VKD", "Ford", "Focus", 2.4)));
-
-        profiles.add(new Profile(
-            new Address("test3", "test3", "test3", "test3", "test3"),
-            new Customer("Stu", "Hayes"),
-            new Car("E57 VKD", "Ford", "Focus", 2.4)));
-
-        return profiles;
     }
 }
--- src/main/java/ProfileDao.java
+++ src/main/java/ProfileDao.java
@@ -1,5 +1,12 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -16,6 +23,19 @@ public class ProfileDao implements Dao<Profile> {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public List<Profile> getProfilesFromCSV(List<Profile> profiles) throws IOException {
+        Reader in = new FileReader("./resources/MOCK_DATA.csv");
+        Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader().parse(in);
+        records.forEach(record -> {
+            profiles.add(new Profile(
+                new Address(record.get("houseNumber"), record.get("street"), record.get("town"), record.get("city"), record.get("postcode")),
+                new Customer(record.get("firstName"), record.get("lastName")),
+                new Car(record.get("carRegistration"), record.get("carMake"), record.get("carModel"), Double.valueOf(record.get("carEngineSize")) )
+            ));
+        });
+
+        return profiles;
     }
 }
