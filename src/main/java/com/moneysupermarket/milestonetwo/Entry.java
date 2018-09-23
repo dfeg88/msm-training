f3a7d6d597f374c0e547f1c33e9107e78a55e888
Daniel Fegan <daniel.fegan@moneysupermarket.com>
2018-09-23 16:44:03 +0100

    refactor MongoConfiguration class

--- src/main/java/com/moneysupermarket/milestonetwo/Entry.java
+++ src/main/java/com/moneysupermarket/milestonetwo/Entry.java
@@ -1,16 +1,38 @@
 package com.moneysupermarket.milestonetwo;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+
+import org.bson.Document;
 
 import com.moneysupermarket.milestonetwo.client.MongoConfiguration;
+import com.moneysupermarket.milestonetwo.dao.CsvDao;
+import com.moneysupermarket.milestonetwo.models.Profile;
+import com.moneysupermarket.milestonetwo.util.FileUtil;
 
 public class Entry {
+
+    private static final String COLLECTION_PROFILE = "profile";
+
 
     public static void main (String[] args) throws IOException {
 //        ProfileDao profileDao = new ProfileDao(new Document());
 //        CsvDao csvDao = new CsvDao(new FileReader(FileUtil.getCsvFile("MOCK_DATA")));
 //        List<Profile> profiles = csvDao.getProfilesFromCSV();
 //        profiles.forEach(profile -> profileDao.save(profile));
-        MongoConfiguration.mongoTestConfig();
+        MongoConfiguration mongoConfiguration = new MongoConfiguration();
+        mongoConfiguration.getDbCollection().drop();
+
+        List<Profile> profiles;
+
+        CsvDao csvDao = new CsvDao(new FileReader(FileUtil.getCsvFile("MOCK_DATA")));
+        profiles = csvDao.getProfilesFromCSV();
+
+        profiles.forEach(profile -> {
+            mongoConfiguration.getDbCollection().insertOne(new Document(COLLECTION_PROFILE, profile));
+        });
+
+        mongoConfiguration.getMongoClient().close();
     }
 }
--- src/main/java/com/moneysupermarket/milestonetwo/client/MongoConfiguration.java
+++ src/main/java/com/moneysupermarket/milestonetwo/client/MongoConfiguration.java
@@ -3,10 +3,7 @@ package com.moneysupermarket.milestonetwo.client;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.bson.Document;
 import org.bson.codecs.configuration.CodecRegistry;
@@ -17,44 +14,32 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.moneysupermarket.milestonetwo.dao.CsvDao;
-import com.moneysupermarket.milestonetwo.models.Profile;
-import com.moneysupermarket.milestonetwo.util.FileUtil;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class MongoConfiguration {
 
-    private static final String COLLECTION_PROFILES = "profiles";
+    private MongoClient mongoClient;
+    private CodecRegistry pojoCodecRegistry;
+    private MongoCollection<Document> dbCollection;
 
-    public static void mongoTestConfig() throws IOException {
-        MongoClient mongoClient = MongoClients.create();
+    public MongoConfiguration() throws IOException {
+        mongoTestConfig();
+    }
 
-        CodecRegistry pojoCodecRegistry =
+    public void mongoTestConfig() throws IOException {
+        mongoClient = MongoClients.create();
+
+        pojoCodecRegistry =
             fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
             fromProviders(PojoCodecProvider.builder().automatic(true).build()));
 
         MongoDatabase database = mongoClient.getDatabase("test")
             .withCodecRegistry(pojoCodecRegistry);
 
-        MongoCollection<Document> collection = database.getCollection("profiles");
-        collection.drop();
-
-        List<Profile> profiles;
-
-        CsvDao csvDao = new CsvDao(new FileReader(FileUtil.getCsvFile("MOCK_DATA")));
-        profiles = csvDao.getProfilesFromCSV();
-
-        List<Document> documents = new ArrayList<>();
-
-        profiles.forEach(profile -> {
-            documents.add(new Document(COLLECTION_PROFILES, profile));
-            System.out.println("Added " + profile.getCustomer().getFirstName() + " " +
-                profile.getCustomer().getLastName() + " to DB");
-        });
-
-        collection.insertMany(documents);
-
-        mongoClient.close();
-
+        dbCollection = database.getCollection("profiles");
     }
 
 }
