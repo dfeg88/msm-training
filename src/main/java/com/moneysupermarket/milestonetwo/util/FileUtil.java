6de54a56ab439c2e7e811fe908292586ee7d44d9
Daniel Fegan <daniel.fegan@moneysupermarket.com>
2018-09-23 16:07:27 +0100

    .

--- src/main/java/com/moneysupermarket/milestonetwo/util/FileUtil.java
+++ src/main/java/com/moneysupermarket/milestonetwo/util/FileUtil.java
@@ -1,6 +1,6 @@
 package com.moneysupermarket.milestonetwo.util;
 
-public abstract  class FileUtil {
+public class FileUtil {
     public static String getCsvFile(String fileName) {
         return "./src/resources/" + fileName + ".csv";
     }
