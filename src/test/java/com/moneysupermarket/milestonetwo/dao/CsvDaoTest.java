74bcb18a2d8bde6995ba2ccff78733be86e1d59e
Daniel Fegan <daniel.fegan@moneysupermarket.com>
2018-09-26 09:15:46 +0100

    wip - csvDao unit tests

--- src/test/java/com/moneysupermarket/milestonetwo/dao/CsvDaoTest.java
+++ src/test/java/com/moneysupermarket/milestonetwo/dao/CsvDaoTest.java
@@ -1,13 +1,16 @@
-import com.moneysupermarket.milestonetwo.dao.CsvDao;
+package com.moneysupermarket.milestonetwo.dao;
+
 import com.moneysupermarket.milestonetwo.models.Address;
 import com.moneysupermarket.milestonetwo.models.Car;
 import com.moneysupermarket.milestonetwo.models.Customer;
 import com.moneysupermarket.milestonetwo.models.Profile;
 import com.moneysupermarket.milestonetwo.util.FileUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.FileNotFoundException;
@@ -22,6 +25,7 @@ public class CsvDaoTest {
 
     private CsvDao csvDao;
     private FileUtil fileUtil = new FileUtil();
+
 
     @BeforeEach
     void setUp() throws FileNotFoundException {
@@ -40,4 +44,24 @@ public class CsvDaoTest {
         List<Profile> actualProfile = csvDao.getProfilesFromCSV();
         assertThat(actualProfile).contains(expectedProfile);
     }
+
+    @Test
+    @DisplayName("List should not contain profile that does not exist")
+    void shouldNotContainProfileThatIsNotInList() throws IOException {
+        Profile expectedProfile = new Profile(
+                new Address("8", "Roxbury", "Haukipudas", "Keda", "SK11 3ED"),
+                new Car("1G6DG577580960863","BMW","Z4",28.8),
+                new Customer("Kate", "DDDDD")
+        );
+
+        List<Profile> actualProfile = csvDao.getProfilesFromCSV();
+        assertThat(actualProfile).doesNotContain(expectedProfile);
+    }
+
+    @Test
+    void shouldThrowIOException() {
+        Assertions.assertThrows(IOException.class, () -> csvDao.getProfilesFromCSV());
+    }
+
+
 }
