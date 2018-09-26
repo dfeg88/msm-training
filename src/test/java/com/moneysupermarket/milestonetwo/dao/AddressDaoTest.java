f4eb14bbe4afb47c052ce5c14b50945e786fce0c
Daniel Fegan <daniel.fegan@moneysupermarket.com>
2018-09-26 11:07:40 +0100

    WIP: unit tests for addressDao

--- src/test/java/com/moneysupermarket/milestonetwo/dao/AddressDaoTest.java
+++ src/test/java/com/moneysupermarket/milestonetwo/dao/AddressDaoTest.java
@@ -1,21 +1,44 @@
 package com.moneysupermarket.milestonetwo.dao;
 
+import com.moneysupermarket.milestonetwo.data.MongoConnection;
+import com.moneysupermarket.milestonetwo.data.MongoProperties;
+import com.moneysupermarket.milestonetwo.models.Address;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 class AddressDaoTest{
 
+    private AddressDao addressDao;
+
+    @Mock
+    private MongoConnection mongoConnection;
+
+    @Mock
+    private Address address;
+
     @BeforeEach
     void setUp() {
+        MongoProperties mongoProperties = MongoProperties.builder()
+                .collection("addresses")
+                .database("msm-training-test")
+                .build();
+
+        mongoConnection = new MongoConnection(mongoProperties, Address.class);
     }
 
     @AfterEach
     void tearDown() {
+        mongoConnection.getMongoClient().close();
     }
 
     @Test
-    void save() {
+    void test_save_shouldWriteAddressesToDatabase() {
+        Assertions.assertNotNull(mongoConnection.getDatabase());
+        Assertions.assertNotNull(mongoConnection.getDbCollection());
+
     }
 
     @Test
