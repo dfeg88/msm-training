e23fbb63ad591989893368005f9b82f5e29849e1
Daniel Fegan <daniel.fegan@moneysupermarket.com>
2018-09-27 09:25:29 +0100

    refactorings

--- src/test/java/com/moneysupermarket/milestonetwo/dao/ProfileDaoTest.java
+++ src/test/java/com/moneysupermarket/milestonetwo/dao/ProfileDaoTest.java
@@ -1,13 +1,18 @@
 package com.moneysupermarket.milestonetwo.dao;
 
+import com.moneysupermarket.milestonetwo.models.Address;
+import com.moneysupermarket.milestonetwo.models.Car;
+import com.moneysupermarket.milestonetwo.models.Customer;
 import com.moneysupermarket.milestonetwo.models.Profile;
 import com.mongodb.client.MongoCollection;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.mockito.Mockito.verify;
@@ -17,15 +22,13 @@ import static org.mockito.Mockito.when;
 class ProfileDaoTest {
 
     private ProfileDao underTest;
+    private List<Profile> profiles = new LinkedList<>();
 
     @Mock
     MongoCollection<Profile> mongoCollection;
 
     @Mock
     private Profile profile;
-
-    @Mock
-    private List<Profile> profiles;
 
     @BeforeEach
     void setUp() {
@@ -40,7 +43,15 @@ class ProfileDaoTest {
 
     @Test
     void getAll() {
-//        when(underTest.getAll()).thenReturn(profiles);
+        profiles.add(new Profile(
+                new Address("test1", "test1", "test1", "test1", "test1"),
+                new Car("E57 VKD", "Ford", "Focus", 2.4),
+                new Customer("Dan", "Fegan")
+        ));
+
+        when(underTest.getAll()).thenReturn(profiles);
+        Assertions.assertNotNull(underTest.getAll());
+//        verify(mongoCollection).find();
     }
 
     @Test
